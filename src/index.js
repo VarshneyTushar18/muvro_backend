1427b7d2cd40615c1920cc4abf3a7b39faa0ef0d
VarshneyTushar18 <tushar.varshney@tech2globe.net>
2026-04-18 11:38:29 +0530

    Refactor index.js to clean up commented code and maintain structure for future development

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 'use strict';
 
-module.exports = {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {},
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/*{ strapi }*/) {},
+// export default {
+ 
+// };
+
+module.exports = {
+  // /**
+  //  * An asynchronous register function that runs before
+  //  * your application is initialized.
+  //  *
+  //  * This gives you an opportunity to extend code.
+  //  */
+  // register(/*{ strapi }*/) {},
+
+  // /**
+  //  * An asynchronous bootstrap function that runs before
+  //  * your application gets started.
+  //  *
+  //  * This gives you an opportunity to set up your data model,
+  //  * run jobs, or perform some special logic.
+  //  */
+  // bootstrap(/*{ strapi }*/) {},
+
+   register({ strapi }) {
+    // Force the socket to be treated as encrypted for proxy setups
+    strapi.server.use(async (ctx, next) => {
+      if (ctx.req?.socket) {
+        (ctx.req.socket as any).encrypted = true;
+      }
+      await next();
+    });
+  },
+
+  bootstrap() {},
 };
