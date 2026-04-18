e90a49c0f10425f08371dfbd1b2efa379a21b322
VarshneyTushar18 <tushar.varshney@tech2globe.net>
2026-04-18 11:35:33 +0530

    Refactor session middleware to check secure connection based on proxy headers; remove unused proxy-secure middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,11 +6,10 @@ module.exports = ({ env }) => [
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
-  'global::proxy-secure',
   {
     name: 'strapi::session',
     config: {
-      secure: (ctx) => ctx.secure,
+      secure: (ctx) => ctx.get('X-Forwarded-Proto') === 'https',
     },
   },
   'strapi::favicon',
